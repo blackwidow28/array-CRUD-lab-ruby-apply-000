8454b4d9931ee7a483c22cde1030e0de4d2b63c3
masterful-messaging-7603 <alicebabatunde15@gmail.com>
2018-09-29 16:41:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,29 @@
 def create_an_empty_array
-  []
+  return []
 end
 
 def create_an_array
-  ["one","two","three","four"]
+  return ["one","two","three","four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push(element)
+  return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
+  return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
+  return array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift()
+  return array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
